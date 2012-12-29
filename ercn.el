67ea455ab1d0fd7397d0a105a93792d484876f3c
David Leatherman <david@leathekd.com>
2012-12-28 22:27:28 -0500

    1.0.1

--- ercn.el
+++ ercn.el
@@ -4,7 +4,7 @@
 
 ;; Author: David Leatherman <leathekd@gmail.com>
 ;; URL: http://www.github.com/leathekd/ercn
-;; Version: 1.0.0
+;; Version: 1.0.1
 
 ;; This file is not part of GNU Emacs.
 
@@ -24,6 +24,8 @@
 ;; History
 
 ;; 1.0.0 - Initial release.  It probably even works.
+
+;; 1.0.1 - save-excursion, to avoid messing with the current line
 
 ;;; License:
 
